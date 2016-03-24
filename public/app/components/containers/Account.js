62189253f411699ad3b40b95cab42ac3f37dcd68
dankwon234 <dan.kwon234@gmail.com>
2016-03-24 16:17:55 -0400

    set up order form

--- public/app/components/containers/Account.js
+++ public/app/components/containers/Account.js
@@ -66,9 +66,9 @@ var Account = React.createClass({
 
 								<ul className="tab-nav clearfix">
 									<li><a href="#tabs-6">Account</a></li>
-									<li><a href="#tabs-7">Order History</a></li>
+									<li><a href="#tabs-7">Place Order</a></li>
 									<li className="hidden-phone">
-										<a href="#tabs-8">Place Order</a>
+										<a href="#tabs-8">Order History</a>
 									</li>
 								</ul>
 
@@ -109,8 +109,33 @@ var Account = React.createClass({
 
 									</div>
 									<div className="tab-content clearfix" id="tabs-7">
-										<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
-										Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.
+
+										<div className="col_two_third nobottommargin">
+
+											<div className="well well-lg nobottommargin">
+												<form id="login-form" name="login-form" className="nobottommargin" action="#" method="post">
+
+													<h3>Place Your Order</h3>
+
+													<div className="col_full">
+														<label>Order:</label>
+														<textarea className="form-control"></textarea>
+													</div>
+
+													<div className="col_full">
+														<label>Adddress:</label>
+														<input type="text" id="lastName" name="login-form-username" className="form-control" />
+													</div>
+
+													<div className="col_full nobottommargin">
+														<button className="button button-3d nomargin" id="login-form-submit" name="login-form-submit" value="update">Submit Order</button>
+													</div>
+
+												</form>
+											</div>
+
+										</div>
+
 									</div>
 									<div className="tab-content clearfix" id="tabs-8">
 										Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.
