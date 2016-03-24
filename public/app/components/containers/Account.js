0ba85834d80d0a4a68fe61aa01039504b67436a8
dankwon234 <dan.kwon234@gmail.com>
2016-03-24 12:47:21 -0400

    account management section

--- public/app/components/containers/Account.js
+++ public/app/components/containers/Account.js
@@ -15,15 +15,48 @@ var Account = React.createClass({
 							<div className="tabs tabs-bordered clearfix" id="tab-2">
 
 								<ul className="tab-nav clearfix">
-									<li><a href="#tabs-6">Nunc tincidunt</a></li>
-									<li><a href="#tabs-7">Proin dolor</a></li>
-									<li className="hidden-phone"><a href="#tabs-8">Aenean lacinia</a></li>
+									<li><a href="#tabs-6">Account</a></li>
+									<li><a href="#tabs-7">Order History</a></li>
+									<li className="hidden-phone">
+										<a href="#tabs-8">Place Order</a>
+									</li>
 								</ul>
 
 								<div className="tab-container" style={{background:'#fff'}}>
 
 									<div className="tab-content clearfix" id="tabs-6">
-										Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.
+
+										<div className="col_two_third nobottommargin">
+
+											<div className="well well-lg nobottommargin">
+												<form id="login-form" name="login-form" className="nobottommargin" action="#" method="post">
+
+													<h3>Manage your Account</h3>
+
+													<div className="col_full">
+														<label for="login-form-username">First Name:</label>
+														<input type="text" id="login-form-username" name="login-form-username" value="" className="form-control" />
+													</div>
+
+													<div className="col_full">
+														<label for="login-form-username">Last Name:</label>
+														<input type="text" id="login-form-username" name="login-form-username" value="" className="form-control" />
+													</div>
+
+													<div className="col_full">
+														<label for="login-form-password">Password:</label>
+														<input type="password" id="login-form-password" name="login-form-password" value="" className="form-control" />
+													</div>
+
+													<div className="col_full nobottommargin">
+														<button className="button button-3d nomargin" id="login-form-submit" name="login-form-submit" value="update">Update</button>
+													</div>
+
+												</form>
+											</div>
+
+										</div>
+
 									</div>
 									<div className="tab-content clearfix" id="tabs-7">
 										<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
