3b16310e082a2e365bcfebafcb6de03e044e7bbe
dankwon234 <dan.kwon234@gmail.com>
2016-03-24 11:41:10 -0400

    identify which page in app.js

--- public/app/App.js
+++ public/app/App.js
@@ -6,7 +6,20 @@ var Register = require('./components/Register');
 var Home = require('./components/containers/Home');
 var Account = require('./components/containers/Account');
 
+
+
 var App = React.createClass({
+	componentWillMount: function(){
+		var path = window.location.pathname.replace('/', ''); // http://localhost:3000/
+		var page = 'home';
+
+		if (path.length > 0){ // not a home page request
+			var parts = path.split('/');
+			page = parts[0];
+		}
+
+		console.log('componentWillMount == '+page);
+	},
 
 	render: function(){
 		return(
