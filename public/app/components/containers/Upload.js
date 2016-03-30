0a0fdf66e1643d04a17e6e8ffb3fe8a79d235a4a
dankwon234 <dan.kwon234@gmail.com>
2016-03-30 11:14:38 -0400

    encType

--- public/app/components/containers/Upload.js
+++ public/app/components/containers/Upload.js
@@ -6,7 +6,7 @@ var Upload = React.createClass({
 		return (
 			<div className="container">
 				<h1>Upload</h1>
-				<form action="/upload/test" method="post" enctype="multipart/form-data">
+				<form action="/upload/test" method="post" encType="multipart/form-data">
 				    <p>Public ID: <input type="text" name="title"/></p>
 				    <p>Image: <input type="file" name="image"/></p>
 				   	<p><input type="submit" value="Upload"/></p>
