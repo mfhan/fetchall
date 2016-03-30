a6c4a1889e7a69b331b39800ec5bd08c15e3ba23
jdrichardstech <jdrichardstech@gmail.com>
2016-03-30 00:48:54 -0400

    edit account page UI

--- public/app/components/containers/Account.js
+++ public/app/components/containers/Account.js
@@ -108,19 +108,19 @@ var Account = React.createClass({
 
 		return (
 			<div>
-		        <section id="content" style={{background: '#f9f9f9'}}>
+		        <section id="content" style={{backgroundImage: 'url("images/account_bkg3.jpg")',}}>
 
-		            <div className="content-wrap">
+		            <div className="content-wrap" style={{padding:0}}>
 
 		                <div className="container clearfix">
-							<h4>Welcome {this.state.currentUser.firstName.toUpperCase() }!</h4>
+							<h1 ><span style={{color:'#fff',textShadow:'1px 1px #000'}}>Welcome {this.state.currentUser.firstName.toUpperCase() }!</span></h1>
 							<div className="tabs tabs-bordered clearfix" id="tab-2">
 
-								<ul className="tab-nav clearfix">
-									<li><a href="#tabs-6">Account</a></li>
-									<li><a href="#tabs-7">Place Order</a></li>
+								<ul className="tab-nav clearfix" style={{background:'#fef1df'}}>
+									<li><a href="#tabs-6" style ={{background:'#fef1df',color:'#000'}}>Account</a></li>
+									<li><a href="#tabs-7" style ={{background:'#fef1df',color:'#000'}}>Place Order</a></li>
 									<li className="hidden-phone">
-										<a href="#tabs-8">Your Orders</a>
+										<a href="#tabs-8" style ={{background:'#fef1df',color:'#000'}}>Your Orders</a>
 									</li>
 								</ul>
 
@@ -130,24 +130,24 @@ var Account = React.createClass({
 
 										<div className="col_two_third nobottommargin">
 
-											<div className="well well-lg nobottommargin">
-												<form id="login-form" name="login-form" className="nobottommargin" action="#" method="post">
+											<div className="well well-lg nobottommargin" style={{border:'2px dotted red'}}>
+												<form id="login-form" name="login-form" className="nobottommargin" action="#" method="post" >
 
-													<h3>Manage your Account</h3>
+													<h3 style={{color:'red'}}>Manage your Account</h3>
 
 													<div className="col_full">
 														<label>First Name:</label>
-														<input type="text" onChange={this.updateCurrentUser} id="firstName" name="login-form-username" value={this.state.currentUser.firstName} className="form-control" />
+														<input type="text" style ={{backgroundColor:'#fff', color:'#000'}} onChange={this.updateCurrentUser} id="firstName" name="login-form-username" value={this.state.currentUser.firstName} className="form-control" />
 													</div>
 
 													<div className="col_full">
 														<label>Last Name:</label>
-														<input type="text" onChange={this.updateCurrentUser} id="lastName" name="login-form-username" value={this.state.currentUser.lastName} className="form-control" />
+														<input type="text" style ={{backgroundColor:'#fff', color:'#000'}} onChange={this.updateCurrentUser} id="lastName" name="login-form-username" value={this.state.currentUser.lastName} className="form-control" />
 													</div>
 
 													<div className="col_full">
 														<label>Password:</label>
-														<input type="password" id="login-form-password" name="login-form-password" value={this.state.currentUser.password} className="form-control" />
+														<input type="password" style ={{backgroundColor:'#fff', color:'#000'}} id="login-form-password" name="login-form-password" value={this.state.currentUser.password} className="form-control" />
 													</div>
 
 													<div className="col_full nobottommargin">
@@ -160,14 +160,14 @@ var Account = React.createClass({
 										</div>
 
 									</div>
-									<div className="tab-content clearfix" id="tabs-7">
+									<div className="tab-content clearfix" id="tabs-7" >
 
 										<div className="col_two_third nobottommargin">
 
-											<div className="well well-lg nobottommargin">
+											<div className="well well-lg nobottommargin" style={{border:'2px dotted red'}}>
 												<form id="login-form" name="login-form" className="nobottommargin" action="#" method="post">
 
-													<h3>Place Your Order</h3>
+													<h3 style={{color:'red'}}>Place Your Order</h3>
 
 													<div className="col_full">
 														<label>Order Here:</label>
@@ -192,7 +192,7 @@ var Account = React.createClass({
 									<div className="tab-content clearfix" id="tabs-8">
 
 										<table className="table">
-										  <thead>
+										  <thead style={{color:'red'}}>
 											<tr>
 											  <th>#</th>
 											  <th>Order</th>
@@ -200,7 +200,7 @@ var Account = React.createClass({
 											  <th>Status</th>
 											</tr>
 										  </thead>
-										  <tbody>
+										  <tbody style={{color:'#555'}}>
 										  	  {orderList}
 										  </tbody>
 										</table>
