2ffaf00830b58ac3320daef6f925464f0968c54f
dankwon234 <dan.kwon234@gmail.com>
2016-03-30 11:29:15 -0400

    misc

--- routes/upload.js
+++ routes/upload.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 var fs = require('fs');
 var cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: 'sample', 
+  api_key: '874837483274837', 
+  api_secret: 'a676b67565c6767a6767d6767f676fe1' 
+});
 
 // var stream = cloudinary.uploader.upload_stream(function(result) { console.log(result); });
 // var file_reader = fs.createReadStream('my_picture.jpg', {encoding: 'binary'}).on('data', stream.write).on('end', stream.end);
@@ -11,7 +16,7 @@ router.post('/:resource', function(req, res, next) {
 //    res.render('index', { title: 'Express' });
 
 	var stream = cloudinary.uploader.upload_stream(function(result) {
-	    console.log(result);
+	    console.log('RESULT: '+result);
 
 	    res.send('Done:<br/> <img src="' + result.url + '"/><br/>' +
 	             cloudinary.image(result.public_id, { format: "png", width: 100, height: 130, crop: "fill" }));
