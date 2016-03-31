e7c104e56d19a2252fb6e6ef3e02b69f975ca39f
mfrance <mf212mf@gmail.com>
2016-03-31 15:09:47 -0400

    final fix on sendemail function

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -59,19 +59,37 @@ module.exports = {
 			    return;
 			}
 
-			else {
-				var path = 'public/email/welcome.html';
-				FileManager.fetchFile(path)
-				.then(function(data){  // comes from "resolve (data)" in the promise
-					var html = data.replace('{{user}}', profile.firstName);
-					EmailManager.sendBatchEmail('mf212mf@gmail.com', profile.email, 'Welcome to fetch', html, null);
-				})
-				.catch(function(err){
-				});
-			}
+			var path = 'public/email/welcome.html';
+			FileManager.fetchFile(path)
+			.then(function(data){
+				var html = data.replace('{{name}}', profile.firstName);
+				EmailManager.sendEmail('info@thegridmedia.com', profile.email, 'Welcome To Fetch!', html, null);
+			})
+			.catch(function(err){
 
+			});
 			completion(null, profile.summary());
 		});
+
+		// Profile.create(params, function(err, profile){
+		// 	if (err){
+		// 		completion(err, null);
+		// 	    return;
+		// 	}
+
+		// 	else {
+		// 		var path = 'public/email/welcome.html';
+		// 		FileManager.fetchFile(path)
+		// 		.then(function(data){  // comes from "resolve (data)" in the promise
+		// 			var html = data.replace('{{user}}', profile.firstName);
+		// 			EmailManager.sendBatchEmail('mf212mf@gmail.com', profile.email, 'Welcome to fetch', html, null);
+		// 		})
+		// 		.catch(function(err){
+		// 		});
+		// 	}
+
+		// 	completion(null, profile.summary());
+		// });
 	},
 
 	// post: function(params, completion){
